c2de700c78df90170f1b43b30b763cfaf3156a72
rajkumar <rajkumar@gmail.com>
2020-05-27 21:43:28 +0530

    add one comment in models file of book app

--- libraryapp/book/models.py
+++ libraryapp/book/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 #DataFlair Models
 class Book(models.Model):
+    #add comment
     name = models.CharField(max_length = 50)
     picture = models.ImageField()
     author = models.CharField(max_length = 30, default='anonymous')
